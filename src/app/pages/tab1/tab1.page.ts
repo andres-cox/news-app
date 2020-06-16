e13f2367b9004388438b0b42744f248f3badaa1f
andres-cox-mojix-com <andreshcox@gmail.com>
2020-06-16 17:54:36 -0300

    tab1 update finished regions, countries added

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -10,38 +10,37 @@ import { IonSegment } from "@ionic/angular";
   styleUrls: ["tab1.page.scss"]
 })
 export class Tab1Page implements OnInit {
-  region: string = "America";
-  country: string = "ar";
-  countries = [];
-  countriesAll: CountryISO[] = [];
+
   newsApiCountries = ["ae", "ar", "at", "au", "be", "bg", "br", "ca", "ch", "cn", "co", "cu", "cz", "de", "eg", "fr", "gb", "gr", "hk", "hu", "id", "ie", "il", "in", "it", "jp", "kr", "lt", "lv", "ma", "mx", "my", "ng", "nl", "no", "nz", "ph", "pl", "pt", "ro", "rs", "ru", "sa", "se", "sg", "si", "sk", "th", "tr", "tw", "ua", "us", "ve", "za"]
   regions = ["America", "Africa", "Asia", "Europe", "Oceania"];
 
+  countries: CountryISO[] = [];
+  countriesAll: CountryISO[] = [];
+  newsCountry: string;
   news: Article[] = [];
 
-  @ViewChild('country') segmentCountries: IonSegment;
+  @ViewChild('region', { static: true }) segmentRegion;
+  @ViewChild('country', { static: true }) segmentCountries;
 
 
   constructor(private newsService: NewsService,
     private httpClient: HttpClient) { }
 
   ngOnInit() {
-    this.segmentCountries.value = 'Argentina';
-    this.loadNews(this.country);
 
     this.httpClient.get('./assets/ISO-3166-countries.json').subscribe(
       data => {
-        this.countries = data as string[];
-        this.countriesAll = this.countries.filter(el => this.newsApiCountries.includes(el.alpha_2.toLowerCase()));
-        this.countries = this.countriesAll;
+        const countries = data as CountryISO[];
+        this.countriesAll = countries.filter(el => this.newsApiCountries.includes(el.alpha_2.toLowerCase()));
+        this.segmentRegion.value = this.regions[0];
+        this.countries = this.countriesAll.filter(el => el.region == this.regions[0]);
+        this.segmentCountries.value = this.countries[0];
+        this.loadNews(this.segmentCountries.value.alpha_2.toLowerCase());
+        this.newsCountry = this.segmentCountries.value.name;
       }
     );
 
   }
-
-  // loadData(event) {
-  //   this.loadNews(event);
-  // }
 
   changeRegion(event) {
     this.countries = this.countriesAll.filter(el => el.region == event.detail.value);
@@ -49,26 +48,11 @@ export class Tab1Page implements OnInit {
 
   changeCountry(event) {
     this.news = [];
+
     this.loadNews(event.detail.value.alpha_2.toLowerCase());
-    console.log(event.detail.value.alpha_2)
+
+    this.newsCountry = event.detail.value.name;
   }
-
-  // loadNews(country: string, event?) {
-  //   this.newsService.getTopHeadlines(country).subscribe(resp => {
-  //     // if (resp.articles.length === 0) {
-  //     //   event.target.disabled = true;
-  //     //   event.target.complete();
-  //     //   return;
-  //     // }
-
-  //     // this.news = resp.articles;
-  //     this.news.push(...resp.articles);
-
-  //     if (event) {
-  //       event.target.complete();
-  //     }
-  //   });
-  // }
 
   loadNews(country: string, event?) {
     this.newsService.getTopHeadlines(country).subscribe(resp => {
@@ -81,6 +65,6 @@ export class Tab1Page implements OnInit {
   }
 
   loadData(event) {
-    this.loadNews(this.country, event);
+    this.loadNews(this.segmentCountries.value.alpha_2.toLowerCase(), event);
   }
 }
